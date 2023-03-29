5d1cef276d11dd65b062b080bfc5d43a65ecc2ae
CVrgas <usual389@gmail.com>
2023-03-29 15:52:48 -0400

    Log In / Sign Up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const C_header = document.getElementById('Cover_header');
+    // this.Cover_header.innerText = 'Existing user?';
+    C_header.innerHTML = 'Sign Up';
+  }
+  cover = document.getElementById('cover');
+  Cover_header = document.getElementById('Coverletter')
+
   title = 'login';
+  todo = "Create Account";
+  position: Boolean = true;
+  
+
+  movecard(){
+    const cover = document.getElementById('cover');
+    const C_header = document.getElementById('Cover_header')
+    this.position = !this.position;
+    switch(!this.position){
+      case true:
+        cover.classList.toggle('MoveLeft', false)
+        cover.classList.toggle('MoveRight', true)
+        C_header.innerHTML = 'Existing user?'
+        break;
+      
+      case false:
+        cover.classList.toggle('MoveRight', false)
+        cover.classList.toggle('MoveLeft', true)
+        C_header.innerHTML = 'Sign Up'
+        break;
+    }
+  }
+  
 }
